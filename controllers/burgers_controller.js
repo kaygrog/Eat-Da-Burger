f3542d033285efe0c1c82c4c60ced469293dbc47
ggank <13955056+kaygrog@users.noreply.github.com>
2020-06-08 21:50:48 -0500

    Complete initial setup of project

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+// TODO
+// Create the router for the app, and export the router at the end of your file.
