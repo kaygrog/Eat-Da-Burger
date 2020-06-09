80a93481fb3079264d25bc26524f7c7723f612f4
ggank <13955056+kaygrog@users.noreply.github.com>
2020-06-08 22:05:39 -0500

    Made app function at bare minimum for heroku deployment

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,3 +6,15 @@ var burger = require("../models/burger.js");
 
 // TODO
 // Create the router for the app, and export the router at the end of your file.
+router.get("/", function (req, res) {
+  // burger.all(function (data) {
+  //   var hbsObject = {
+  //     burgers: data,
+  //   };
+  //   console.log(hbsObject);
+  //   res.render("index", hbsObject);
+  // });
+  res.render("index");
+});
+
+module.exports = router;
